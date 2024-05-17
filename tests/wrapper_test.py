6b77b783384c46a5b41ea963bd96f53e6cfacaa1
Mayank Bhargava <mbhargava.gd@gmail.com>
2024-05-17 17:35:17 +0530

    add transform package

---
+++ tests/wrapper_test.py
@@ -0,0 +1,22 @@
+import unittest
+from typing import Any, Dict
+
+from gdtransform.transform import transformation
+
+
+@transformation(name='my-transformation')
+def add_field(data: Dict[str, Any]):
+    data['field'] = 'value'
+
+
+class WrapperTest(unittest.TestCase):
+
+    def test_wrapper(self):
+        self.assertEqual(True, getattr(add_field, '__gd_transformation__'))
+        self.assertEqual('my-transformation', getattr(add_field, '__gd_transformation_name__'))
+
+    def test_transform(self):
+        data = {}
+        add_field(data)
+        self.assertTrue('field' in data)
+        self.assertEqual('value', data['field'])
